dd2184b78648f685affcc817ef60df805c68f01d
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-10 03:07:01 +0600

    0-what is console.log()?.js

---
+++ The Console/0-what is console.log()?.js
@@ -0,0 +1,11 @@
+/*In JavaScript, the console.log() method tells the web console to display a message
+We will write our desired message to display inside the parentheses with opening and closing quotation
+We have to keep in mind to use ';'at the end of the parentheses!
+*/ 
+
+//This is an example of a console.log() method:
+
+console.log("👋 Hi!");
+console.log("I am Noblesse");
+console.log("Nice to meet you!");
+
