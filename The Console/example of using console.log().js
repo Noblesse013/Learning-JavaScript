e1ad7ce69b0d74dd8d6a325ba874e71a0fc98129
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-10 03:18:12 +0600

    example of using console.log().js

---
+++ The Console/example of using console.log().js
@@ -0,0 +1,29 @@
+/*In order to output to look exactly  this, we have to type exactly like this inside the console.log()
+==================================
+         McDONALD'S RECEIPT       
+----------------------------------
+           Order Details          
+----------------------------------
+Item           Quantity   Price($)
+----------------------------------
+🍔 Burger       1          2.99   
+🍟 Fries        1          2.49   
+🥤 Soda         1          1.99   
+----------------------------------
+Total                      7.47   
+==================================
+*/
+
+console.log("=================================");
+console.log("           McDONALD'S RECEIPT    ");
+console.log("---------------------------------");
+console.log("           Order Details         ");
+console.log("---------------------------------");
+console.log("Item          Quantity   Price($)");
+console.log("---------------------------------");
+console.log("🍔 Burger      1           2.99  ");
+console.log("🍟 Fries       1           2.49   ");
+console.log("🥤 Soda        1           1.99   ");
+console.log("---------------------------------");
+console.log("Total                      7.47   ");
+console.log("=================================");
