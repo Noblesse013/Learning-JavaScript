cf1d313a7359c7ad0702b5e2ec3edd1f303dd446
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-10 14:24:47 +0600

    Create Declaring variables.js

---
+++ Variables/Declaring variables.js
@@ -0,0 +1,13 @@
+//keyword variableName = value
+
+const userId = 1618033988;
+let userName = "Mehreen Mallick Fiona";
+let progress = 0.69;
+let xp = 13;
+let verified = true;
+
+console.log(userId);
+console.log(userName);
+console.log(progress);
+console.log(xp);
+console.log(verified);
