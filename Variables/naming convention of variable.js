3b3e9dd33306a7d4d1392eaf6928654811ffc95a
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-10 14:39:06 +0600

    Create naming convention of variable.js

---
+++ Variables/naming convention of variable.js
@@ -0,0 +1,5 @@
+/*JavaScript variable names are usually written in camelCase. 
+camelCase is a naming convention in which the first letter of each word is capitalized, except for the first word.*/
+
+const userId = 013;
+let userName = "Noblesse";
